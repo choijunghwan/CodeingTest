c12e348bbcb6d5af1f6e8f0cdb1391d7cee62cd2
ChoiJungHwan <vkdlxj3562@naver.com>
2021-04-26 18:57:29 +0900

    [프로그래머스] 탐욕법 Greedy6

--- src/Greedy/Greedy6.java
+++ src/Greedy/Greedy6.java
@@ -1,7 +1,7 @@
 package Greedy;
 
-
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class Greedy6 {
     public static void main(String[] args) {
@@ -13,12 +13,37 @@ public class Greedy6 {
     }
 
     public static int solution(int[][] routes) {
-        Arrays.sort(routes);
+        Arrays.sort(routes,
+                new Comparator<int[]>(){
+
+                    @Override
+                    public int compare(int[] o1, int[] o2) {
+                        return o1[0] < o2[0] ? -1 : 1;
+                    }
+                });
 
         for (int[] route : routes){
-            System.out.println(route[0] + route[1]);
+            System.out.println(route[0] + " " + route[1]);
         }
 
-        return 0;
+        int start = routes[0][0];
+        int end = routes[0][1];
+        int count = 1;
+
+        for (int[] route : routes){
+
+            if (route[0] <= end) {
+                if (end > route[1]) {
+                    end = route[1];
+                }
+                start = route[0];
+            } else {
+                start = route[0];
+                end = route[1];
+                count++;
+            }
+        }
+
+        return count;
     }
 }
