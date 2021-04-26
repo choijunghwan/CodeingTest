347571039dedefcdfcdc78d0107df09090bb59f4
ChoiJungHwan <vkdlxj3562@naver.com>
2021-04-26 18:27:19 +0900

    merge 전 임시 버전

---
+++ src/Greedy/Greedy5.java
@@ -0,0 +1,17 @@
+package Greedy;
+
+import java.util.*;
+
+public class Greedy5 {
+    public static void main(String[] args) {
+        int n = 4;
+        int[][] costs = {{0, 1, 2}, {0, 2, 2}, {0, 3, 1}, {1, 2, 5}, {1, 3, 1}, {2, 3, 8}};
+
+        int answer = solution(n, costs);
+        System.out.println("answer = " + answer);
+    }
+
+    public static int solution(int n, int[][] costs) {
+
+    }
+}
