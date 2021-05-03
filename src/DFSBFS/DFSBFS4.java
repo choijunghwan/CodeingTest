24166ce77b71730d9a60741654e54c1c6ff9f943
ChoiJungHwan <vkdlxj3562@naver.com>
2021-05-03 17:52:45 +0900

    [프로그래머스] DFS/BFS 4

--- src/DFSBFS/DFSBFS4.java
+++ src/DFSBFS/DFSBFS4.java
@@ -2,30 +2,78 @@ package DFSBFS;
 
 import java.util.*;
 
+/**
+ * 나의 코드와 구글링 코드의 차이점은
+ * 나는 dfs를 사용할때 일일이 정렬을 하여 삽입하고 route를 찾았다면
+ * 구글링 코드는 모든 경우를 다 찾아서 넣고, 모든 경우들을 모아놓고 정렬을 하여 결과값을 찾았다.
+ */
 public class DFSBFS4 {
+    private static final int SRC = 0;
+    private static final int DST = 1;
+
+    private static ArrayList<String> answer = new ArrayList<>();
+    private static String route = "";
+    private static boolean[] visited;
+
+
     public static void main(String[] args) {
 //        String[][] tickets = {{"ICN", "SFO"}, {"ICN", "ATL"}, {"SFO", "ATL"}, {"ATL", "ICN"}, {"ATL", "SFO"}};
 //        String[][] tickets = {{"ICN","A"},{"ICN","B"},{"B","ICN"}};
 //        String[][] tickets = {{"ICN", "AAA"}, {"ICN", "AAA"}, {"ICN", "AAA"}, {"AAA", "ICN"}, {"AAA", "ICN"}};
-        String[][] tickets = {{"ICN", "B"}, {"B", "ICN"}, {"ICN", "A"}, {"A", "D"}, {"D", "A"}};
+//        String[][] tickets = {{"ICN", "B"}, {"B", "ICN"}, {"ICN", "A"}, {"A", "D"}, {"D", "A"}};
+//        String[][] tickets = {{"ICN", "A"}, {"ICN", "A"}, {"A", "ICN"}, {"A", "C"}};
+        String[][] tickets = {{"ICN", "A"}, {"ICN", "A"}, {"A", "ICN"}};
+
+        /*
         ArrayList<String> answer = new ArrayList<>();
         answer.add("ICN");
-
         boolean[] visited = new boolean[tickets.length];
-
         for (int i = 0; i < tickets.length; i++) {
             visited[i] = false;
         }
-
-        dfs("ICN", visited, answer, tickets);
-
+        self_dfs("ICN", visited, answer, tickets);
         System.out.println("answer = " + answer);
-
         String[] array = answer.toArray(new String[answer.size()]);
+        */
+
+        for (int i = 0; i < tickets.length; i++){
+            visited = new boolean[tickets.length];
+            String src = tickets[i][SRC];
+            String dst = tickets[i][DST];
+
+            if (src.equals("ICN")){
+                route = src + ",";
+                visited[i] = true;
+                dfs(tickets, dst, 1);
+            }
+        }
+        Collections.sort(answer);
 
     }
 
-    static ArrayList<String> dfs(String begin, boolean[] visited, ArrayList<String> answer, String[][] tickets) {
+    static void dfs(String[][] tickets, String dst, int visitCount) {
+        route += dst + ",";
+
+        if (visitCount == tickets.length){
+            answer.add(route);
+            return;
+        }
+
+        for (int i = 0; i < tickets.length; i++) {
+            String nextSrc = tickets[i][SRC];
+            String nextDst = tickets[i][DST];
+
+            if (dst.equals(nextSrc) && !visited[i]) {
+                visited[i] = true;
+                dfs(tickets, nextDst, visitCount+1);
+                visited[i] = false;
+                route = route.substring(0, route.length() - 4);
+            }
+
+        }
+    }
+
+    static ArrayList<String> self_dfs(String begin, boolean[] visited, ArrayList<String> answer, String[][] tickets) {
 
         // 종료조건: 모든 곳을 방문했을경우
         int count = 0;
@@ -60,7 +108,7 @@ public class DFSBFS4 {
             if (check > 0) {
                 visited[entry.getValue()] = true;
                 answer.add(entry.getKey());
-                dfs(entry.getKey(), visited, answer, tickets);
+                self_dfs(entry.getKey(), visited, answer, tickets);
                 break;
             }
 
@@ -68,11 +116,6 @@ public class DFSBFS4 {
                 answer.add(entry.getKey());
             }
         }
-
-//        visited[map.get(map.firstKey())] = true;
-//        answer.add(map.firstKey());
-//        dfs(map.firstKey(), visited, answer, tickets);
-
 
         return answer;
     }
