7db32344521935b4eb8b1576dea77b384720ece8
ChoiJungHwan <vkdlxj3562@naver.com>
2021-05-07 21:39:17 +0900

    [프로그래머스] KAKAO 2020 가사검색 55점 코드

--- src/KAKAO/KAKAO_2020_4.java
+++ src/KAKAO/KAKAO_2020_4.java
@@ -20,4 +20,37 @@ public class KAKAO_2020_4 {
         System.out.println("words = " + words);
         System.out.println("queries = " + queries);
     }
+
+    // 정확성 25점 효율성 30점짜리 코드
+    public static int[] precise_solution(String[] words, String[] queries) {
+        int[] answer = new int[queries.length];
+
+        for (int i = 0; i < queries.length; i++) {
+            String query = queries[i];
+            int count = 0;
+            for (int j = 0; j < words.length; j++) {
+                String word = words[j];
+                boolean check = true;
+                if (query.length() != word.length()) {
+                    continue;
+                }
+
+                for (int index = 0; index < query.length(); index++) {
+                    if (query.charAt(index) == '?') {
+                        continue;
+                    } else if (query.charAt(index) != word.charAt(index)) {
+                        check = false;
+                        break;
+                    }
+                }
+
+                if (check) {
+                    count++;
+                }
+            }
+
+            answer[i] = count;
+        }
+        return answer;
+    }
 }
