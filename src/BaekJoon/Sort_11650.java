f9f763c6a03d4406ce1f111d21598148708a5336
ChoiJungHwan <vkdlxj3562@naver.com>
2021-06-14 18:32:12 +0900

    백준 11650 람다식 사용버전

--- src/BaekJoon/Sort_11650.java
+++ src/BaekJoon/Sort_11650.java
@@ -3,6 +3,7 @@ package BaekJoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
@@ -12,13 +13,32 @@ public class Sort_11650 {
         int num = Integer.parseInt(br.readLine());
 
         PriorityQueue<Edge> pq = new PriorityQueue<>();
+        int[][] arr = new int[num][2];
 
         for (int i = 0; i < num; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
             pq.add(new Edge(x,y));
+            arr[i][0] = x;
+            arr[i][1] = y;
         }
+
+        //람다식을 이용한 정렬
+        //Comparator를 이용한 방법이다.
+        Arrays.sort(arr, (e1,e2) -> {
+            if (e1[0] == e2[0]) {
+                return e1[1] - e2[1];
+            } else {
+                return e1[0] - e2[0];
+            }
+        });
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < num; i++) {
+            sb.append(arr[i][0] + " " + arr[i][1]).append('\n');
+        }
+        System.out.println(sb);
 
         while (!pq.isEmpty()) {
             Edge edge = pq.poll();
