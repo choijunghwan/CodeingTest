840d6570106067e0d7a393f94124b3454614ae64
ChoiJungHwan <vkdlxj3562@naver.com>
2021-06-15 16:32:44 +0900

    백준 10825, 11652

---
+++ src/BaekJoon/Sort_10825.java
@@ -0,0 +1,41 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Sort_10825 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        String[][] arr = new String[n][4];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = br.readLine().split(" ");
+        }
+
+        // 음수 or 0이면 객체의 자리가 그대로 유지되며, 양수인 경우에는 두 객체의 자리가 바뀐다.
+        Arrays.sort(arr, (e1, e2) -> {
+            if (e1[1].equals(e2[1])) {
+                if (e1[2].equals(e2[2])) {
+                    if (e1[3].equals(e2[3])) {
+                        return e1[0].compareTo(e2[0]);
+                    } else {
+                        return Integer.compare(Integer.parseInt(e2[3]), Integer.parseInt(e1[3]));
+                    }
+                } else {
+                    return Integer.compare(Integer.parseInt(e1[2]), Integer.parseInt(e2[2]));
+                }
+            } else {
+                return Integer.compare(Integer.parseInt(e2[1]), Integer.parseInt(e1[1]));
+            }
+        });
+
+        for (int i = 0; i <n; i++) {
+            System.out.println(arr[i][0]);
+        }
+    }
+}
+
---
+++ src/BaekJoon/Sort_11652.java
@@ -0,0 +1,53 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Sort_11652 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        long[] arr = new long[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Long.parseLong(br.readLine());
+        }
+
+        if(n==1) { // n이 1일 경우는 0번 인덱스만 출력해주면 된다.
+            System.out.println(arr[0]);
+            return;
+        }
+        Arrays.sort(arr);
+
+        int cnt = 1;
+        int max = 0;
+        long ans = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            if (i == n - 2) {
+                if (arr[i] == arr[i + 1]) {
+                    cnt++;
+                }
+                if (cnt > max) {
+                    max = cnt;
+                    ans = arr[i];
+                }
+
+            }
+            else if (arr[i] == arr[i + 1]) {
+                cnt++;
+            }
+            else {
+                if (cnt > max) {
+                    max = cnt;
+                    ans = arr[i];
+                }
+                cnt = 1;
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
