207e7c54fef90e3d8e6d2cb1ee15d4446d3bc719
ChoiJungHwan <vkdlxj3562@naver.com>
2021-06-17 17:36:29 +0900

    백준 10844

---
+++ src/BaekJoon/DP_9095.java
@@ -0,0 +1,28 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class DP_9095 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        int[] dp = new int[12];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+        dp[4] = 7;
+
+        for (int i = 5; i < 12; i++) {
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+        }
+
+        for (int i = 0; i < num; i++) {
+            int temp = Integer.parseInt(br.readLine());
+            System.out.println(dp[temp]);
+        }
+
+    }
+}
