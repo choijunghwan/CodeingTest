276a0b8fba9f6927f925a3a5533705e883869f57
choijunhwan <vkdlxj3562@naver.com>
2021-06-20 02:00:23 +0900

    백준 2193

---
+++ src/BaekJoon/DP_2193.java
@@ -0,0 +1,29 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class DP_2193 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+
+        long[][] dp = new long[num+1][2];
+
+        dp[1][0] = 0;
+        dp[1][1] = 1;
+
+        for (int i = 2; i <= num; i++) {
+            dp[i][0] = dp[i-1][0] + dp[i-1][1];
+            dp[i][1] = dp[i-1][0];
+        }
+
+        long result = 0;
+        for (int i = 0; i < 2; i++) {
+            result += dp[num][i];
+        }
+
+        System.out.println(result);
+    }
+}
