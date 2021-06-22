e3b4afe86e3b6e6f7a4a8623bded4b966f80de92
choijunhwan <vkdlxj3562@naver.com>
2021-06-22 23:58:05 +0900

    백준 10799

---
+++ src/BaekJoon/Sort/Stack_10799.java
@@ -0,0 +1,34 @@
+package BaekJoon.Sort;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Stack_10799 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        Stack<Character> stack = new Stack<>();
+        int answer = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            char temp = str.charAt(i);
+
+            if (temp == '(') {
+                stack.add('(');
+            } else if (temp == ')') {
+                if (str.charAt(i - 1) == ')') {
+                    stack.pop();
+                    answer++;
+                } else {
+                    stack.pop();
+                    answer += stack.size();
+                }
+            }
+        }
+
+        System.out.println(answer);
+
+    }
+}
