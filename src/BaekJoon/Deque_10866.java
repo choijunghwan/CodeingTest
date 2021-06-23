776518aff13e3d3d24a00aa466124eccb007de89
choijunhwan <vkdlxj3562@naver.com>
2021-06-24 00:09:28 +0900

    백준 10808

--- src/BaekJoon/Deque_10866.java
+++ src/BaekJoon/Deque_10866.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Deque_10866 {
@@ -12,7 +13,6 @@ public class Deque_10866 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         list = new ArrayList<>();
-
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
