fc83e48fc0dba77e0ba92c90e7967f153c922001
choijunhwan <vkdlxj3562@naver.com>
2021-06-25 17:32:10 +0900

    백준 11656

---
+++ src/BaekJoon/String_11656.java
@@ -0,0 +1,24 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class String_11656 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        int length = str.length();
+        String[] arr = new String[length];
+
+        for (int i = 0; i < length; i++) {
+            arr[i] = str.substring(i,length);
+        }
+        Arrays.sort(arr);
+
+        for (int i = 0; i < length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
