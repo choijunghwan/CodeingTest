6d2e66127a9c220ed8a9528ceccfe371039f6798
choijunhwan <vkdlxj3562@naver.com>
2021-06-27 13:14:32 +0900

    백준 1934

---
+++ src/BaekJoon/String_1934.java
@@ -0,0 +1,30 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class String_1934 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            String[] str = br.readLine().split(" ");
+            int a = Integer.parseInt(str[0]);
+            int b = Integer.parseInt(str[1]);
+
+            int d = gcd(a, b);
+
+            System.out.println(a * b / d);
+        }
+    }
+
+    private static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+
+        return gcd(b, a % b);
+    }
+}
