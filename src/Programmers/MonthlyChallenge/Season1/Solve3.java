f3854c2aac48c384fb3250e7b5973dcaf2b64d73
ChoiJungHwan <vkdlxj3562@naver.com>
2021-08-12 14:37:24 +0900

    [프로그래머스] 월간 코드 챌린지 시즌1 두 개 뽑아서 더하기

---
+++ src/Programmers/MonthlyChallenge/Season1/Solve3.java
@@ -0,0 +1,18 @@
+package Programmers.MonthlyChallenge.Season1;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solve3 {
+    public int[] solution(int[] numbers) {
+        Set<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < numbers.length -1; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                set.add(numbers[i] + numbers[j]);
+            }
+        }
+
+        return set.stream().sorted().mapToInt(Integer :: intValue).toArray();
+    }
+}
