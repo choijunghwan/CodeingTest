d33306d1f1eb775dc359032150812cdb8f818a7a
choijunhwan <vkdlxj3562@naver.com>
2021-08-25 16:08:27 +0900

    [프로그래머스]위클리 챌린지 4주차 update

--- src/Programmers/WeeklyChallenge/Solve4.java
+++ src/Programmers/WeeklyChallenge/Solve4.java
@@ -37,6 +37,33 @@ public class Solve4 {
         return result[0][0];
     }
 
+    public static String solutionV2(String[] table, String[] languages, int[] preference) {
+        String answer = "";
+        int score = -1;
+
+        for (String str : table) {
+            String[] t = str.split(" ");
+            String tname = t[0];
+            int tscore = 0;
+            for (int i = 0; i < languages.length; i++) {
+                int idx = Arrays.asList(t).indexOf(languages[i]);
+                if (idx > -1) {
+                    tscore += preference[i] * (6 - idx);
+                }
+            }
+
+            if (score == tscore && answer.compareTo(tname) > 0) {
+                answer = tname;
+            }
+
+            if (score < tscore) {
+                score = tscore;
+                answer = tname;
+            }
+        }
+        return answer;
+    }
+
     static class CustomComparator implements Comparator<String[]> {
 
         @Override
