a0335d563a3ea3b964e5c45b5f8d6255db9acefa
choijunhwan <vkdlxj3562@naver.com>
2021-09-29 16:39:03 +0900

    백준 1753

---
+++ src/BaekJoon/Gold/Gold_1753.java
@@ -0,0 +1,88 @@
+package BaekJoon.Gold;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Gold_1753 {
+    static int V;
+    static int E;
+    static int K;
+    static int maps[][];
+    static List<Node>[] list;
+    static int[] dist;
+    static final int INF = 100_000_000;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        V = Integer.parseInt(st.nextToken());
+        E = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(br.readLine());
+        list = new ArrayList[V + 1];
+        dist = new int[V + 1];
+
+        Arrays.fill(dist, INF);
+        for (int i = 1; i <= V; i++) {
+            list[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            list[u].add(new Node(v, w));
+        }
+
+        dijkstra(K);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= V; i++) {
+            if (dist[i] == INF) {
+                sb.append("INF\n");
+            } else {
+                sb.append(dist[i] + "\n");
+            }
+        }
+        System.out.println(sb.toString());
+
+    }
+
+    private static void dijkstra(int start) {
+        PriorityQueue<Node> queue = new PriorityQueue<>();
+        boolean[] check = new boolean[V + 1];
+        queue.add(new Node(start, 0));
+        dist[start] = 0;
+
+        while (!queue.isEmpty()) {
+            Node curNode = queue.poll();
+            int cur = curNode.end;
+
+            if (check[cur]) continue;
+            check[cur] = true;
+            for (Node node : list[cur]) {
+                if (dist[node.end] > dist[cur] + node.weight) {
+                    dist[node.end] = dist[cur] + node.weight;
+                    queue.add(new Node(node.end, dist[node.end]));
+                }
+            }
+        }
+    }
+
+    static class Node implements Comparable<Node>{
+        int end;
+        int weight;
+
+        public Node(int end, int weight) {
+            this.end = end;
+            this.weight = weight;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return weight - o.weight;
+        }
+    }
+}
