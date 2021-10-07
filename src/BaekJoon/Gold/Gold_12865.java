7c548df0e70110b0697bebdc57cb3c634d8babbd
choijunhwan <vkdlxj3562@naver.com>
2021-10-07 16:34:21 +0900

    백준12865

---
+++ src/BaekJoon/Gold/Gold_12865.java
@@ -0,0 +1,44 @@
+package BaekJoon.Gold;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Gold_12865 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int[] weight = new int[N + 1];
+        int[] worth = new int[N + 1];
+        int[][] dp = new int[N + 1][K + 1];
+
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            weight[i] = Integer.parseInt(st.nextToken());
+            worth[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= K; j++) {
+
+                // i번째 무게를 더 담을 수 없는 경우
+                if (weight[i] > j) {
+                    dp[i][j] = dp[i - 1][j];
+                }
+                // i번째 무게를 더 담을 수 있는 경우
+                else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - weight[i]] + worth[i]);
+                }
+            }
+        }
+
+        System.out.println(dp[N][K]);
+
+    }
+
+
+}
