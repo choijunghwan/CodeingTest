71c06bae1d5903168cba8d39e520b57efba66a8d
choijunhwan <vkdlxj3562@naver.com>
2021-10-25 16:47:31 +0900

    네이버ì웹툰 라이브코딩 리팩토링

--- src/TestCodeReview/NaverWebtoon/Solve1.java
+++ src/TestCodeReview/NaverWebtoon/Solve1.java
@@ -1,5 +1,8 @@
 package TestCodeReview.NaverWebtoon;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Solve1 {
 
     /**
@@ -8,20 +11,31 @@ public class Solve1 {
      * aczefacce -> z
      * abcdfabcd -> f
      */
+
+    /**
+     * 문자열에서 문자가 나온 순서대로 Queue에 넣어준다.
+     * Queue에서 하나씩 꺼내면서 유일한 문자를 탐색한다.
+     */
     public static void main(String[] args) {
         String input = "abcdfabcd";
         String answer = null;
 
+        Queue<Character> queue = new LinkedList<>();
+
         int[] arr = new int[26];
         for (int i = 0; i < input.length(); i++) {
             int index = input.charAt(i) - 'a';
+            if (arr[index] == 0) {
+                queue.add(input.charAt(i));
+            }
             arr[index]++;
         }
 
-        for (int i = 0; i < input.length(); i++) {
-            int index = input.charAt(i) - 'a';
-            if (arr[index] == 1) {
-                answer = String.valueOf(input.charAt(i));
+
+        while (!queue.isEmpty()) {
+            Character poll = queue.poll();
+            if (arr[poll - 'a'] == 1) {
+                answer = String.valueOf(poll);
                 break;
             }
         }
